4c33a894a6109bfb94695523f0f28c4c1fad56ff
matthewmarshdev <matthewmarshdev@gmail.com>
2018-02-21 08:41:16 -0700

    wednesday morning commit

--- bamazonSchema.sql
+++ bamazonSchema.sql
@@ -13,39 +13,44 @@ CREATE TABLE product (
   PRIMARY KEY (id)
 );
 
-INSERT INTO product (product_name, department_name, price, stock_quantity)
-VALUES ("simpsonwave_vol_1", "trap_vidz", 29.99, 100);
+INSERT INTO product ( product_name, department_name, price, stock_quantity)
+VALUES ('fantasywave', 'trap_vidz', 24.99, 167);
 
 INSERT INTO product (product_name, department_name, price, stock_quantity)
-VALUES ("simpsonwave_vol_2", "trap_vidz", 27.99, 1000);
+VALUES ('zelda_plushie', 'toys', 21.99, 600);
 
 INSERT INTO product (product_name, department_name, price, stock_quantity)
-VALUES ("simpsonwave_vol_3", "trap_vidz", 21.99, 1237);
+VALUES ('link_plushie', 'toys', 21.99, 589);
 
 INSERT INTO product (product_name, department_name, price, stock_quantity)
-VALUES ("simpsonwave_vol_4", "trap_vidz", 1364.00, 5);
+VALUES ('simpsonwave_vol_4', 'trap_vidz', 1364.00, 5);
 
 INSERT INTO product (product_name, department_name, price, stock_quantity)
-VALUES ("trappin_in_japan_anthology", "set", 999.00, 7);
+VALUES ('trappin_in_japan_anthology', 'set', 999.00, 7);
 
 INSERT INTO product (product_name, department_name, price, stock_quantity)
-VALUES ("furry_headband", "clothing", 24.89, 50);
+VALUES ('furry_headband', 'clothing', 24.89, 50);
 
 INSERT INTO product (product_name, department_name, price, stock_quantity)
-VALUES ("rick_drool_shirt", "clothing", 23.24, 43);
+VALUES ('rick_drool_shirt', 'clothing', 23.24, 43);
 
 INSERT INTO product (product_name, department_name, price, stock_quantity)
-VALUES ("homer_sprinkle_donut", "clothing", 12.99, 400);
+VALUES ('homer_sprinkle_donut', 'clothing', 12.99, 400);
 
 INSERT INTO product (product_name, department_name, price, stock_quantity)
-VALUES ("simpsonwave_package", "set", 9999.99, 3);
+VALUES ('simpsonwave_package', 'set', 9999.99, 3);
 
 INSERT INTO product (product_name, department_name, price, stock_quantity)
-VALUES ("rickwave_package", "set", 7999.99, 3);
+VALUES ('rickwave_package', 'set', 7999.99, 3);
 
 INSERT INTO product (product_name, department_name, price, stock_quantity)
-VALUES ("tokyo_bus_pass", "collectable", 199.99, 15);
+VALUES ('tokyo_bus_pass', 'collectable', 199.99, 15);
 
 INSERT INTO product (product_name, department_name, price, stock_quantity)
-VALUES ("all_trapz_kit", "set", 69999.99, 1);
+VALUES ('all_trapz_kit', 'set', 69999.99, 1);
 
+
+
+SELECT * FROM product;
+
+SELECT * FROM product WHERE department_name = 'toys';
